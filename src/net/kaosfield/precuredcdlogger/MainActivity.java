00706f59e2bf2e3d7f45f0c6145b15fb62a97fa1
ka <ka.kaosf@gmail.com>
2013-06-30 16:41:27 +0900

    implement WebView

--- src/net/kaosfield/precuredcdlogger/MainActivity.java
+++ src/net/kaosfield/precuredcdlogger/MainActivity.java
@@ -3,13 +3,22 @@ package net.kaosfield.precuredcdlogger;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 
 public class MainActivity extends Activity {
+
+  private WebView wv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    wv = (WebView) findViewById(R.id.webview);
+    wv.getSettings().setJavaScriptEnabled(true);
+    wv.setWebChromeClient(new WebChromeClient());
+    wv.loadUrl("file:///android_asset/index.html");
   }
 
   @Override
