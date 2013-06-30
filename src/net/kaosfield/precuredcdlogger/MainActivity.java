4c90a69d9488e1ab7619ef5320395a82830791ed
ka <ka.kaosf@gmail.com>
2013-06-30 16:56:46 +0900

    enable to go back with back button

--- src/net/kaosfield/precuredcdlogger/MainActivity.java
+++ src/net/kaosfield/precuredcdlogger/MainActivity.java
@@ -2,6 +2,7 @@ package net.kaosfield.precuredcdlogger;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebView;
 import android.webkit.WebChromeClient;
@@ -19,6 +20,18 @@ public class MainActivity extends Activity {
     wv.getSettings().setJavaScriptEnabled(true);
     wv.setWebChromeClient(new WebChromeClient());
     wv.loadUrl("file:///android_asset/index.html");
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    // Check if the key event was the Back button and if there's history
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
+      wv.goBack();
+      return true;
+    }
+    // If it wasn't the Back key or there's no web page history, bubble up to the default
+    // system behavior (probably exit the activity)
+    return super.onKeyDown(keyCode, event);
   }
 
   @Override
